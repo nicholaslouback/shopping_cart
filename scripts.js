b1bef5f41cda20ace1081d9b5f6e452b210c77b4
nicholaslouback <nilouback@hotmail.com>
2024-09-02 08:20:25 -0300

    feat: add Event

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const removeProduct = document.getElementsByClassName("remove_product")
 console.log(removeProduct)
 for (var i = 0 ; i < removeProduct.length; i++) {
-    removeProduct[i].addEventListener("click", function() {
-    console.log("clicou")
+    removeProduct[i].addEventListener("click", function(event) {
+    event.target.parentElement.remove()
     })
     }
